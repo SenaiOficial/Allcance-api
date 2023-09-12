80c1cd1366cc33a8917248b13bcd6537c130bb33
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-09-12 20:40:21 -0300

    create users model

---
+++ laravel/app/Models/Users.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    protected $fillabel = [
+        'first_name',
+        'last_name',
+        'phone_number',
+        'cpf',
+        'date_of_birth',
+        'marital_status',
+        'gender',
+        'email',
+        'password'
+    ];
+    
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function getAge()
+    {
+        return now()->diffInYears($this->date_of_birth);
+    }
+}
