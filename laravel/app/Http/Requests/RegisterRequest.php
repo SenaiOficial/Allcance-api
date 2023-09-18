56a5a3fc0c2a972f8bbbff00c9eb2e5fc085e99f
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-09-17 22:44:10 -0300

    ajustes

--- laravel/app/Http/Requests/RegisterRequest.php
+++ laravel/app/Http/Requests/RegisterRequest.php
@@ -17,7 +17,7 @@ class RegisterRequest extends FormRequest
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
             'phone_number' => ['required', 'string', 'max:20'],
-            'cpf' => ['required', 'string', 'cpf', 'unique:users'],
+            'cpf' => ['required'],
             'date_of_birth' => ['required', 'date'],
             'marital_status' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'string', 'max:255'],
@@ -28,6 +28,24 @@ class RegisterRequest extends FormRequest
                 'min:8',
                 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/',
             ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'O campo nome é obrigatório.',
+            'last_name.required' => 'O campo sobrenome é obrigatório.',
+            'phone_number.required' => 'O campo número de telefone é obrigatório.',
+            'cpf.required' => 'O campo CPF é obrigatório.',
+            'cpf.unique' => 'Este CPF já está em uso.',
+            'date_of_birth.required' => 'O campo data de nascimento é obrigatório.',
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'Digite um endereço de email válido.',
+            'email.unique' => 'Este email já está em uso.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.min' => 'A senha deve ter pelo menos :min caracteres.',
+            'password.regex' => 'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.',
         ];
     }
 }
