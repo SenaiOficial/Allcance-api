3a1fd21ecf9ccb7838e880023adb41ea354fd520
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-09-23 00:39:52 -0300

    ajustes

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -22,31 +23,50 @@ class UserController extends Controller
     }
 
     public function store(RegisterRequest $request)
-{
-    try {
-        $validatedData = $request->validated();
-        
-        $validatedData['password'] = Hash::make($validatedData['password']);
-        
-        $user = User::create($validatedData);
+    {
+        try {
+            $validatedData = $request->validated();
+            
+            $validatedData['password'] = Hash::make($validatedData['password']);
+            
+            $user = User::create($validatedData);
 
-        if (!$user) {
-            Log::error('Erro ao criar usuário: Falha na criação do usuário');
-            return response()->json(['error' => 'Erro ao criar usuário1'], 500);
+            if (!$user) {
+                Log::error('Erro ao criar usuário: Falha na criação do usuário');
+                return response()->json(['error' => 'Erro ao criar usuário1'], 500);
+            }
+
+            Log::info('Usuário criado com sucesso');
+            return response()->json(['message' => 'Usuário criado com sucesso']);
+        } catch (\Exception $e) {
+            Log::error('Erro ao criar usuário: ' . $e->getMessage());
+            return response()->json(['errors' => $e->getMessage()], 500);
+        }    
+    }
+
+    protected function getErrors($validator)
+    {
+        $errors = [];
+
+        if ($validator->errors()->has('date_of_birth')) {
+            $errors['date_of_birth'] = $validator->errors()->first('date_of_birth');
+        }
+    
+        if ($validator->errors()->has('email')) {
+            $errors['email'] = $validator->errors()->first('email');
+        }
+    
+        if ($validator->errors()->has('cpf')) {
+            $errors['cpf'] = $validator->errors()->first('cpf');
         }
 
-        Log::info('Usuário criado com sucesso');
-        return response()->json(['message' => 'Usuário criado com sucesso']);
-    } catch (\Exception $e) {
-        Log::error('Erro ao criar usuário: ' . $e->getMessage());
-        return response()->json(['errors' => $e->getMessage()], 500);
-    }    
-}
+        return $errors;
+    }
 
     public function update(RegisterRequest $request, User $user)
     {
         $validatedData = $request->validated();
-        
+
         $user->update($validatedData);
 
         return response()->json(['message' => 'Usuário atualizado com sucesso']);
