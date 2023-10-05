fc3a67d23915e63ce81022dc7ff55dfd8863bd5d
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-10-04 22:06:04 -0300

    feat addresses migaration and model

---
+++ laravel/app/Models/Address.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $table = 'addresses';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'cep',
+        'country',
+        'state',
+        'city',
+        'street',
+        'street_number',
+        'street_complement'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function scopeCity($query, $city)
+    {
+        return $query->where('city', $city);
+    }
+}
