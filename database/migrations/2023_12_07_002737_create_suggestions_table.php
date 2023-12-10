507115b3ef706e31bf15de85111ec779ee3d42af
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2023-12-10 19:56:13 -0300

    relacionando

--- database/migrations/2023_12_07_002737_create_suggestions_table.php
+++ database/migrations/2023_12_07_002737_create_suggestions_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('content');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('pcd_users')->onDelete('cascade');
         });
     }
 
