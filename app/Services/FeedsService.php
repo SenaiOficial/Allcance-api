f5ecce7601a4560998db9f0caf2338ada1fad4af
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2024-03-20 07:52:58 -0300

    Update FeedsService.php

--- app/Services/FeedsService.php
+++ app/Services/FeedsService.php
@@ -80,7 +80,7 @@ class FeedsService
         $validateData = $request->validated();
         $image = $request->file('image');
         $filename = $image->hashName();
-        $request->file('image')->move(public_path($this->storage), $filename);
+        $request->file('image')->move(storage_path($this->storage), $filename);
         $validateData['admin_user_id'] = $user->id;
         $validateData['profile_photo'] = $user->profile_photo;
         $validateData['institution_name'] = $user->institution_name;
