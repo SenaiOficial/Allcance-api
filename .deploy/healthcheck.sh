cbbff0eb44b6b53c477407a89726e2679d7b8e02
Tiago Oliveira <113936360+tiagocriar@users.noreply.github.com>
2024-03-21 22:42:01 -0300

    ajust route health check

--- .deploy/healthcheck.sh
+++ .deploy/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ $(curl -o /dev/null -L -s -w "%{http_code}\n" http://localhost/docker-health-check) = "200" ]; then
+if [ $(curl -o /dev/null -L -s -w "%{http_code}\n" http://localhost/api/docker-health-check) = "200" ]; then
     exit 0
 else
     exit 1
