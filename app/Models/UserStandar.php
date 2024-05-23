a1243c4a029b6e29f78d8185d55f1e0dfae8bd69
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2024-05-23 20:54:22 -0300

    clean

--- app/Models/UserStandar.php
+++ app/Models/UserStandar.php
@@ -3,10 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\ResetPassword;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
