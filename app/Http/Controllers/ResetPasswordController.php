56640e81ec9ec7f6d12e11380e1561d535504abe
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2024-10-28 19:06:03 -0300

    ajusts new password message

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -19,8 +19,23 @@ class ResetPasswordController extends Controller
     try {
       $request->validate([
         'password' => 'required',
-        'new_password' => 'required|string|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/|different:password',
+        'new_password' => [
+          'required',
+          'string',
+          'min:8',
+          'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/',
+          'different:password'
+        ],
         'confirm_password' => 'required|same:new_password'
+      ], [
+        'password.required' => 'A senha atual é obrigatória.',
+        'new_password.required' => 'A nova senha é obrigatória.',
+        'new_password.string' => 'A nova senha deve ser um texto.',
+        'new_password.min' => 'A nova senha deve ter no mínimo 8 caracteres.',
+        'new_password.regex' => 'A nova senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.',
+        'new_password.different' => 'A nova senha deve ser diferente da senha atual.',
+        'confirm_password.required' => 'A confirmação da nova senha é obrigatória.',
+        'confirm_password.same' => 'A confirmação da senha deve corresponder à nova senha.'
       ]);
 
       $user = auth($this->guard)->user();
@@ -33,7 +48,6 @@ class ResetPasswordController extends Controller
       $user->save();
 
       return response()->json(['message' => 'Senha alterada com sucesso'], 200);
-
     } catch (\Exception $e) {
       return response()->json(['message' => 'Erro ao redefinir a senha: ' . $e->getMessage()], 500);
     }
